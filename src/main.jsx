eabb1f020f561f3045a3d10fc4e0399e7163d47c
sinitasaare <sinitasaare@gmail.com>
2026-03-08 03:42:57 +1100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App, { ErrorBoundary } from './App';
 import './index.css';
 import './utils/validation.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
